40ea08db9a113fbd5c4fe2bb4485130b590be615
Zalan Vegvari <vegvarizalan@gmail.com>
2021-02-02 10:14:05 +0100

    Shader stage codes collected

--- src/engine/shadercompiler/compile.cpp
+++ src/engine/shadercompiler/compile.cpp
@@ -47,8 +47,8 @@ static bool include_headers(const std::string& filename, std::ostream& out,
                 auto headersBegin =
                   std::sregex_iterator(headerContent->begin(), headerContent->end(), headerReg);
                 auto headersEnd = std::sregex_iterator();
-                for (std::sregex_iterator i = headersBegin; i != headersEnd; ++i) {
-                    std::string headerId = (*i)[0];
+                for (std::sregex_iterator regI = headersBegin; regI != headersEnd; ++regI) {
+                    std::string headerId = (*regI)[0];
                     auto itr = headerPaths.find(headerId);
                     if (itr == headerPaths.end()) {
                         std::cerr << "Could not find header: " << headerId << std::endl;
@@ -75,6 +75,34 @@ static bool include_headers(const std::string& filename, std::ostream& out,
     return ret;
 }
 
+static bool collect_shader(const BlockFile& blockFile, std::ostream& out, const std::string& type) {
+    for (size_t i = 0; i < blockFile.getBlockCount("global"); ++i) {
+        const BlockFile* b = blockFile.getNode("global", i);
+        if (b->hasContent()) {
+            out << *b->getContent();
+        }
+        else if (b->hasNodes()) {
+            // completely empty block is allowed
+            std::cerr << "A shader block (global) contains nested blocks instead of content."
+                      << std::endl;
+            return false;
+        }
+    }
+    for (size_t i = 0; i < blockFile.getBlockCount(type); ++i) {
+        const BlockFile* b = blockFile.getNode(type, i);
+        if (b->hasContent()) {
+            out << *b->getContent();
+        }
+        else if (b->hasNodes()) {
+            // completely empty block is allowed
+            std::cerr << "A shader block (" << type
+                      << ") contains nested blocks instead of content." << std::endl;
+            return false;
+        }
+    }
+    return true;
+}
+
 bool compile_shader(const std::string& shaderFile,
                     const std::unordered_map<std::string, fs::path>& headerPaths) {
     std::stringstream cu;
@@ -84,8 +112,31 @@ bool compile_shader(const std::string& shaderFile,
         std::cerr << "Could not collect headers for shader: " << shaderFile << std::endl;
         return false;
     }
+    BlockFile cuBlocks(cu, false);
+    if (!cuBlocks.hasNodes()) {
+        std::cerr << "Compilation unit doesn't have any blocks: " << shaderFile << std::endl;
+        return false;
+    }
+    std::stringstream vs;
+    std::stringstream ps;
+    std::stringstream cs;
 
-    std::cout << cu.str() << std::endl;
+    if (!collect_shader(cuBlocks, vs, "vs")) {
+        std::cerr << "Could not collect vs shader content in: " << shaderFile << std::endl;
+        return false;
+    }
+    if (!collect_shader(cuBlocks, ps, "ps")) {
+        std::cerr << "Could not collect ps shader content in: " << shaderFile << std::endl;
+        return false;
+    }
+    if (!collect_shader(cuBlocks, cs, "cs")) {
+        std::cerr << "Could not collect cs shader content in: " << shaderFile << std::endl;
+        return false;
+    }
+
+    std::cout << "vs: " << vs.str() << std::endl;
+    std::cout << "ps: " << ps.str() << std::endl;
+    std::cout << "cs: " << cs.str() << std::endl;
 
     return true;
 }
