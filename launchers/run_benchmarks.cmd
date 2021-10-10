5268525887cd0ac220e1d98a78b087f725959bdf
Zalan Vegvari <vegvarizalan@gmail.com>
2021-10-10 22:03:56 +0200

    Configuration for static benchmarks

--- launchers/run_benchmarks.cmd
+++ launchers/run_benchmarks.cmd
@@ -7,6 +7,22 @@ IF NOT exist %resulultsFolder% ( mkdir %resulultsFolder% )
 SET CurrentDir=%resulultsFolder%\%DATE:/=-%_%TIME::=-%
 mkdir %CurrentDir%
 
+@REM Warmup run
+SET CurrentBenchmark=clean_control
+SET EngineOption=..\data\engineOptions\benchmark_clean_control.json
+SET Scene=..\data\scenes\clean_benchmark_scene.json
+"../build/src/games/playground/Playground.exe" -c../data/configs/playground_default.json -t../data/configs/trackingConfig.json -d../data/ --scene %Scene% --options %EngineOption% --no_persistance --runtime_stats_persistance statistics/runtimeStats.bin --runtime_stats_game_exports statistics/runtimeStatsGameExports.bin --runtime_stats_cache statistics/runtimeStatsCache.bin -s../build/shaders/shaders.bin --report statistics/report.txt
+  if errorlevel 1 goto error
+SET BenchmarkFolder=%CurrentDir%\%CurrentBenchmark%
+mkdir %BenchmarkFolder%
+python ../scripts/generate_plots.py benchmarks/benchmark.csv %BenchmarkFolder%
+XCOPY /q logs\all.log %BenchmarkFolder%
+XCOPY /q %Scene% %BenchmarkFolder%
+XCOPY /q %EngineOption% %BenchmarkFolder%
+SET CleanControlDownsampled=%BenchmarkFolder%\downsampled.csv
+@REM Warmup run
+
+
 SET CurrentBenchmark=clean_control
 SET EngineOption=..\data\engineOptions\benchmark_clean_control.json
 SET Scene=..\data\scenes\clean_benchmark_scene.json
